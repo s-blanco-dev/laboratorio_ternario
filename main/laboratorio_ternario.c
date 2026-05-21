e9a36ab4be44da68a72ff57f6e3919fe3d1f3c17
Felipe <felipaladino05@gmail.com>
2026-05-21 19:58:44 -0300

    irrelevant

--- main/laboratorio_ternario.c
+++ main/laboratorio_ternario.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/semphr.h"
+
+SemaphoreHandle_t xMutex = NULL;
 
 void app_main(void)
 {
+  xMutex = xSemaphoreCreateMutexWithCaps(void);
 
+  if (xMutex != NULL){
+    printf("success\n");
+  }
+  else{
+    printf("ball\n");
+  }
 }
