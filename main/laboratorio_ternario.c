20d770c2e8d54e21d18d74fa1c9e26f5d818d7ea
Felipe <felipaladino05@gmail.com>
2026-05-23 20:41:47 -0300

    aaaaa, anda con main viejo

--- main/laboratorio_ternario.c
+++ main/laboratorio_ternario.c
@@ -19,7 +19,7 @@ void app_main(void)
     ESP_ERROR_CHECK(led_rgb_init(&led));
     task_a_params.color_mutex = color_mutex;
     task_a_params.led_strip = led;
-    task_a_params.current_color = murrent;
+    task_a_params.current_color = &murrent;
     xTaskCreate(blink_led_task, "TASK_A", CONFIG_ESP32_PTHREAD_TASK_STACK_SIZE_DEFAULT, &task_a_params, tskIDLE_PRIORITY+1, &task_a_handler);
 
 
